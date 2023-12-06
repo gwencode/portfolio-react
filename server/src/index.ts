59f3bc738e295167bfa470e40cdd05f8003ad171
Gwendal Le Bris <gwen.coding@gmail.com>
2023-12-06 16:54:20 +0100

    Basic interation with database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,34 +1,44 @@
 import app from "./server";
-import { AppDataSource } from "./data-source";
-import { User } from "./entity/User";
-import { Project } from "./entity/Project";
-import { ProjectImage } from "./entity/ProjectImage";
+// import { AppDataSource } from "./data-source";
+// import { User } from "./entity/User";
+// import { Project } from "./entity/Project";
+// import { ProjectImage } from "./entity/ProjectImage";
 
-AppDataSource.initialize()
-  .then(async () => {
-    console.log(
-      "Loading users, projects and project images repositories from the database"
-    );
-    const userRepository = AppDataSource.getRepository(User);
-    const projectRepository = AppDataSource.getRepository(Project);
-    const projectImageRepository = AppDataSource.getRepository(ProjectImage);
+// AppDataSource.initialize()
+//   .then(async () => {
+// console.log(
+//   "Loading users, projects and project images repositories from the database"
+// );
+// const userRepository = AppDataSource.getRepository(User);
+// const projectRepository = AppDataSource.getRepository(Project);
+// const projectImageRepository = AppDataSource.getRepository(ProjectImage);
 
-    // const firstProject = await projectRepository.findOneBy({ id: 1 });
-    // const firstProjectImage = await projectImageRepository.findOneBy({ id: 1 });
+// // const firstProject = await projectRepository.findOneBy({ id: 1 });
+// // const firstProjectImage = await projectImageRepository.findOneBy({ id: 1 });
 
-    const users = await userRepository.find();
-    const projects = await projectRepository.find();
-    const projectImages = await projectImageRepository.find();
+// let users = await userRepository.find();
+// // await userRepository.remove(users);
+// // const user = userRepository.create({
+// //   firstName: "Gwendal",
+// //   lastName: "Le Bris",
+// //   age: 28,
+// // });
+// // await userRepository.save(user);
+// // users = await userRepository.find();
 
-    console.log("Loaded users: ", users);
-    console.log("Loaded projects: ", projects);
-    console.log("Loaded project images: ", projectImages);
+// const projects = await projectRepository.find();
+// const projectImages = await projectImageRepository.find();
 
-    console.log(
-      "Here you can setup and run express / fastify / any other framework."
-    );
-    app.listen(5000, () => {
-      console.log("Listening on http://localhost:5000");
-    });
-  })
-  .catch((error) => console.log(error));
+// console.log("Loaded users: ", users);
+// console.log("Loaded projects: ", projects);
+// console.log("Loaded project images: ", projectImages);
+
+// console.log(
+//   "Here you can setup and run express / fastify / any other framework."
+// );
+// })
+// .catch((error) => console.log(error));
+
+app.listen(5000, () => {
+  console.log("Listening on http://localhost:5000");
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,55 @@
-import express from "express";
-import fs from "fs/promises";
+import express, { Request, Response } from "express";
+// import fs from "fs/promises";
 import "reflect-metadata";
+import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
+import { Project } from "./entity/Project";
+import { ProjectImage } from "./entity/ProjectImage";
 
+// establish database connection
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
+
+// create and setup express app
 const app = express();
+app.use(express.json());
 
+// register routes
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get("/projects", async (req, res) => {
+app.get("/projects", async (req: Request, res: Response) => {
   try {
-    // Get json file in folder database/db.json
-    const jsonData = await fs.readFile("./database/db.json", "utf-8");
-    // Parse json file
-    const data = JSON.parse(jsonData);
-    // Extract "projects" from json file
-    const projects = data.projects;
-    // Send projects to client
+    const projects = await AppDataSource.getRepository(Project).find();
     res.json(projects);
   } catch (error) {
-    console.error("Error while getting projects from db.json", error);
+    console.error("Error while getting projects from database", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const users = await AppDataSource.getRepository(User).find();
+    res.json(users);
+  } catch (error) {
+    console.error("Error while getting users from database", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  try {
+    const user = await AppDataSource.getRepository(User).save(req.body);
+    res.json(user);
+  } catch (error) {
+    console.error("Error while saving new user into database", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
